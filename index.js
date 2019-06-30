0313e114241033636d6bb8ab1cb7a08dea848b13
hardik <hardik96bansal@gmail.com>
2019-06-30 18:14:52 +0530

    node callback and error

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 var rect = require('./rectangle');
 
 function rectDetails(a,b){
-    console.log("Area is "+rect.area(a,b));
-    console.log("Perimeter is "+rect.perimeter(a,b));
+    console.log("Solving for values " + a + ", " + b);
+    rect(a,b,(err,rectangle) => {
+        if(err){
+            console.log("ERROR: " + err.message);
+        }
+        else{
+            console.log("Area for rectangle[" + a + "," + b + "] is " + rectangle.area());
+            console.log("Perimeter for rectangle[" + a + "," + b + "] is " + rectangle.perimeter());
+        }
+
+    });
 }
 
 rectDetails(2,3);
 rectDetails(4,5);
+rectDetails(4,-5);
--- rectangle.js
+++ rectangle.js
@@ -1,2 +1,16 @@
-exports.area = (x,y) => (x*y);
-exports.perimeter = (x,y) => (2*(x+y))
+module.exports = (a,b,callback) => {
+    if(a<=0 || b<=0){
+        setTimeout(() =>
+            callback(new Error(a + " and " + b + " are invalid values"),null)
+            ,2000);        
+    }
+    else{
+        setTimeout(() =>
+            callback(null,
+                {
+                    area: () => (a*b),
+                    perimeter: () => (2*(a+b))
+                })
+            ,2000);
+    }
+};
